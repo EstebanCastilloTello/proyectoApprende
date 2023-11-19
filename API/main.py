eff29aa3849c4abdad6931cff1fe05302680041c
kamus1 <benjamincamus9@gmail.com>
2023-11-18 21:16:28 -0300

    endpoints insumos

--- API/main.py
+++ API/main.py
@@ -51,8 +51,6 @@ def encontrar_palabra_clave_descripcion(descripcion):
     
     return response.choices[0].text
 
-
-
 ##funcion de los profes
 def buscar_profesores(temaMateria):
 
@@ -66,7 +64,7 @@ def buscar_profesores(temaMateria):
     nombreProfesor = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'landing-v4-ads-pic-firstname')))
     linkPerfilProfesor = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'landing-v4-ads-bloc.tck-announce-link')))
 
-    # Crea una lista de tuplas (nombreProfesor, linkPerfilProfesor)
+    #crear una lista de diccionarios para guardar los profesores
     profesores = []
 
     for i in range(len(nombreProfesor)):
@@ -79,11 +77,95 @@ def buscar_profesores(temaMateria):
     return profesores
 
 
+#-------funcion para encontrar insumos en descripcion usando openai----#
+
+def encontrar_insumos_descripcion(descripcion):
+    prompt = "de la siguiente descripción de talleres, reconoce productos o materiales que se necesiten para la realización del taller, solamente entrega los posibles productos separado por guiones \"-\", ejemplo: producto1-producto2-producto3, la descripcion es:" + descripcion
+
+    response = openai.Completion.create(
+        model='text-davinci-003',
+        prompt=prompt,
+        temperature=0,
+        max_tokens=40,
+        top_p=1,
+        frequency_penalty=0.5,
+        presence_penalty=0
+    )
+
+    # Verificar si la respuesta contiene productos
+    productos_encontrados = response.choices[0].text.strip()
+    
+    if len(productos_encontrados) > 0:
+        return productos_encontrados
+    else:
+        return ""
 
 
-##link para aumentar cantidad buscada en superprof
-#https://www.superprof.cl/s/python,Chile,,,1.html?n=2
-#aumentar el ultimo valor para buscar mas profesores
+def buscar_insumos_lider(insumo, cantidad=1, tiempo_espera=5):
+    # URL del supermercado
+    url_supermercado = "https://www.lider.cl/supermercado/search?query=" + insumo
+
+    # Página a buscar
+    driver.get(url_supermercado)
+    
+    # Espera hasta que se carguen los elementos con la clase "product-card_description-wrapper"
+    wait = WebDriverWait(driver, tiempo_espera)
+    descripcionProducto = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'product-card_description-wrapper')))
+
+    # Obtener nombres de los primeros 'cantidad' productos
+    productos = [element.text for element in descripcionProducto[:cantidad]]
+
+    elementos_ais_hits_item = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'ais-Hits-item')))
+        # Obtener enlaces de los elementos <a> dentro de la clase "ais-Hits-item"
+    enlaces = []
+    for elemento_ais_hits_item in elementos_ais_hits_item[:cantidad]:
+        # Buscar el elemento <a> dentro de cada elemento ais-Hits-item
+        enlace = elemento_ais_hits_item.find_element(By.TAG_NAME, 'a').get_attribute('href')
+        enlaces.append(enlace)
+    
+    #crear una lista de diccionarios para guardar los productos
+    productos_lista = []
+    for i in range(len(productos)):
+        productos_lista.append({"nombre": productos[i], "link": enlaces[i]})
+        
+    
+    return productos_lista
+
+
+
+
+def buscar_insumos_mercadoLibre(insumo, cantidad=1, tiempo_espera=5):
+
+    # URL del proveedor de insumos
+    url_proveedor_insumos = "https://listado.mercadolibre.cl/" + insumo
+
+    # Página a buscar
+    driver.get(url_proveedor_insumos)
+    
+    # Espera hasta que se carguen los elementos con la clase "product-card_description-wrapper"
+    wait = WebDriverWait(driver, tiempo_espera)
+    descripcionProducto = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'ui-search-item__title')))
+
+    
+    # Obtener nombres de los primeros 'cantidad' productos
+    productos = [element.text for element in descripcionProducto[:cantidad]]
+
+    elementos_ais_hits_item = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'ui-search-item__group')))
+    #Obtener enlaces de los elementos <a> dentro de la clase "ui-search-item__group ui-search-item__group--title"
+    enlaces = []
+    for elemento_ais_hits_item in elementos_ais_hits_item[:cantidad]:
+        # Buscar el elemento <a> dentro de cada elemento 
+        enlace = elemento_ais_hits_item.find_element(By.TAG_NAME, 'a').get_attribute('href')
+        enlaces.append(enlace)
+    
+    #crear una lista de diccionarios para guardar los productos
+    productos_lista = []
+    for i in range(len(productos)):
+        productos_lista.append({"nombre": productos[i], "link": enlaces[i]})
+        
+    
+
+    return productos_lista
 
 
 
@@ -140,6 +222,89 @@ def get_profesores(descripcion_taller: str):
         
     return {"profesores": resultado}
 
+
+
+
+#endpoint para encontrar insumos en lider
+@app.get("/insumosLider/{descripcion_taller}")
+def get_insumos(descripcion_taller: str):
+    
+    #llamar a la funcion de encontrar insumos usando openai
+    productosIdentificados = encontrar_insumos_descripcion(descripcion_taller)
+    
+    if(productosIdentificados == ""):
+        return {"insumos": [{"nombre": "No se encontraron productos", "link": "No se encontraron productos"}]}
+    
+    
+    #de la descripcion, separar los productos identificados por guiones y guardarlos en una lista
+    productosIdentificados = productosIdentificados.split("-")
+    
+    #solo si productosIdentificados tiene mas de un producto eliminamos el ultimo elemento
+    #por temas de tokes de openai, probablemente el utlimo producto esté incompleto
+    if(len(productosIdentificados) > 1):
+        productosIdentificados.pop()
+        
+    #lista para guardar los productos encontrados 
+    insumosLider = [] #lista de diccionarios
+    
+    #por cada producto identificado, buscarlo en proveedor de insumos
+    if len(productosIdentificados) > 0:
+        #buscar solo 3 productos si es que hay 3 
+        if len(productosIdentificados) > 3:
+            for i in range(3):
+                insumosLider.append(buscar_insumos_lider(productosIdentificados[i]))
+        else:
+            for i in range(len(productosIdentificados)):
+                insumosLider.append(buscar_insumos_lider(productosIdentificados[i]))
+        
+    
+    else:
+        insumosLider.append({"nombre": "No se encontraron productos", "link": "No se encontraron productos"})
+    
+    #desempaquetar lista de listas de diccionarios
+    lista_diccionarios_insumos = [insumo for elementoInsumo in insumosLider for insumo in elementoInsumo]
+    
+    return {"insumos": lista_diccionarios_insumos}
+
+
+
+#endpoint para encontrar insumos en mercado libre
+@app.get("/insumosMercadoLibre/{descripcion_taller}")
+def get_insumos(descripcion_taller: str):
+            
+    #llamar a la funcion de encontrar insumos usando openai
+    productosIdentificados = encontrar_insumos_descripcion(descripcion_taller)
+    
+    #de la descripcion, separar los productos identificados por guiones y guardarlos en una lista
+    productosIdentificados = productosIdentificados.split("-")
+    
+    #solo si productosIdentificados tiene mas de un producto eliminamos el ultimo elemento
+    #por temas de tokes de openai, probablemente el ultimo producto esté incompleto
+    if(len(productosIdentificados) > 1):
+        productosIdentificados.pop()
+        
+    #lista para guardar los productos encontrados 
+    insumosMercadoLibre = []
+    
+    #por cada producto identificado, buscarlo en proveedor de insumos
+    if len(productosIdentificados) > 0:
+        #buscar solo 3 productos si es que hay 3 
+        if len(productosIdentificados) > 3:
+            for i in range(3):
+                insumosMercadoLibre.append(buscar_insumos_mercadoLibre(productosIdentificados[i]))
+        else:
+            for i in range(len(productosIdentificados)):
+                insumosMercadoLibre.append(buscar_insumos_mercadoLibre(productosIdentificados[i]))
+        
+    else:
+        insumosMercadoLibre.append({"nombre": "No se encontraron productos", "link": "No se encontraron productos"})
+    
+    #desempaquetar lista de listas de diccionarios
+    lista_diccionarios_insumos = [insumo for elementoInsumo in insumosMercadoLibre for insumo in elementoInsumo]
+            
+    return {"insumos": lista_diccionarios_insumos}
+            
+            
 
 #-----------------------------------------------------
 
